fe3b22e142c0abe04f6e7e179bbe841234905461
Harshil-sitapara <harshilsitapara4@gmail.com>
2023-11-15 18:00:22 +0530

    changes in helper.json

--- Client/src/services/Helper.js
+++ Client/src/services/Helper.js
@@ -1,4 +1,5 @@
 //  const BASE_URL = "https://foodease-rfty.onrender.com"
- const BASE_URL = "https://foodease-server.onrender.com"
-// 
- export default BASE_URL
+const BASE_URL =
+  "http://localhost:8080" || "https://foodease-server.onrender.com";
+//
+export default BASE_URL;
