4c10d715ca171fdbc14046ba9851939486699869
Harshil-sitapara <harshilsitapara4@gmail.com>
2024-02-19 13:30:35 +0530

    change CRT994

--- Client/src/services/Helper.js
+++ Client/src/services/Helper.js
@@ -1,3 +1,3 @@
 //  const BASE_URL = "https://foodease-rfty.onrender.com"
-const BASE_URL="https://foodease-server.onrender.com" || "http://localhost:8080";
+const BASE_URL="https://foodease-rfty.onrender.com" || "http://localhost:8080";
 export default BASE_URL;
