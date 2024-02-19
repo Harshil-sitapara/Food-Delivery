a876a26d8493507897f43641fac6e8c0f3002bc0
Harshil-sitapara <harshilsitapara4@gmail.com>
2024-02-19 10:59:06 +0530

    change CRT985

--- Client/src/services/Helper.js
+++ Client/src/services/Helper.js
@@ -1,5 +1,3 @@
 //  const BASE_URL = "https://foodease-rfty.onrender.com"
-const BASE_URL =
-  "http://localhost:8080" || "https://food-delivery-3udtqh8sh-harshils-projects-93c2268a.vercel.app";
-//
+const BASE_URL="https://food-delivery-3udtqh8sh-harshils-projects-93c2268a.vercel.app";
 export default BASE_URL;
